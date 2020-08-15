66537dea1477588938ea3f7bd4b29de5151cc36d
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-08-15 23:40:40 +0900

    2020/08/15

--- Part02/Chapter04_구현/4-1.py
+++ Part02/Chapter04_구현/4-1.py
@@ -5,3 +5,14 @@ print(move)
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
+logue = ['L', 'R', 'U', 'D']
+
+for i in move:
+    for j in range(len(logue)):
+        if i == logue[j]:
+            nx = dx[j] + x
+            ny = dy[j] + y
+    if nx < 1 or ny < 1 or nx > N or ny > N:
+        continue
+    x, y = nx, ny
+print(x, y)
